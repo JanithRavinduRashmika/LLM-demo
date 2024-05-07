5a20c7b12b11adfdfcda9dcf3c4608d0e60b5633
JanithRavinduRashmika <rashmikajanithravindu@gmail.com>
2024-05-07 22:56:50 +0530

    Groq start.

---
+++ groq/app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import os
+from langchain_groq import ChatGroq 
+from langchain_community import WebBaseLoader
+from langchain.embeddings import OllamaEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains import create_retrieval_chain
+
+from dotenv import load_dotenv
+load_dotenv()
+
+groq_api_key = os.environ["GROQ_API_KEY"]
+
+
